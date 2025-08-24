715ecc1ec498849b58bb23e9b6bf9fdec627c8b7
hjkim24 <hjkim4842@naver.com>
2025-08-24 12:32:37 +0900

    feat(be): initialize prettier

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
         process.env.NODE_ENV === 'production' ? '.env' : '.env.development',
       isGlobal: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
